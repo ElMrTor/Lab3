e2725bc0808c5daa14e14932a208276f3eec09a7
4015d10 <4015d10@136.145.57.226>
2017-11-15 14:23:24 -0400

    JFrame con nombre, 200*200 size y centralizado.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,11 +1,18 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		JFrame myFrame = new JFrame("Hector");
+		
+		Dimension screensize = Toolkit.getDefaultToolkit().getScreenSize();
+		int height = (int) screensize.getHeight();
+		int width = (int) screensize.getWidth();
+		
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setLocation(width/2, height/2);
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
 }
